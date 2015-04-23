1e6843bfe62df0688ce4f5afde9778bae13ad1a1
jocelyn alsdorf & cleri philiastides <student@epicodus.com>
2015-04-23 09:59:46 -0700

    Add and pass spec to divide self by 25 and push to an array

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,5 +1,10 @@
 class Fixnum
   define_method(:coin) do
-
+  amount = self
+  q = 25
+  coin_totals = Array.new()
+  num_of_coins = amount./(q)
+  coin_totals.push(num_of_coins)
+  coin_totals
   end
 end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -5,8 +5,8 @@ require('coin')
 #it returns the remainder of self divided by 25
 
 describe('Fixnum#coin') do
-  it("divides self by 25 and pushes the return value to an array")do
-  expect((62).coin.()).to(eq([2]))
+  it("divides self by 25 and pushes the return value to an array") do
+  expect(62.coin()).to(eq([2]))
 end
 
 
