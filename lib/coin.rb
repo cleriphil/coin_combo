0229cda8cde4e4987025b81ef06d9d9a4603d624
jocelyn alsdorf & cleri philiastides <student@epicodus.com>
2015-04-23 10:15:48 -0700

    add and pass spec that tests it returns remainder of self divided by 10 and pushed to array

--- lib/coin.rb
+++ lib/coin.rb
@@ -2,9 +2,12 @@ class Fixnum
   define_method(:coin) do
   amount = self
   q = 25
+  d = 10
   coin_totals = Array.new()
   num_of_coins = amount./(q)
   coin_totals.push(num_of_coins)
-  coin_totals
+  remainder = amount.%(q)
+  num_of_coins = remainder./(d)
+  coin_totals.push(num_of_coins)
   end
 end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -2,12 +2,14 @@ require('rspec')
 require('coin')
 
 #it divides self by 25 pushes the return value to an array to track
-#it returns the remainder of self divided by 25
+#returns the remainder of self divided by 25, divides remainder by 10 and pushes to array"
+
 
 describe('Fixnum#coin') do
   it("divides self by 25 and pushes the return value to an array") do
-  expect(62.coin()).to(eq([2]))
+  expect(50.coin()).to(eq([2, 0]))
 end
-
-
+  it("returns the remainder of self divided by 25, divides remainder by 10 and pushes to array") do
+  expect(60.coin()).to(eq([2, 1]))
+end
 end
